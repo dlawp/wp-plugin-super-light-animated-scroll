086a72fda83dea1dca3be5baa60544edb099196e
Michał Kukla <degregar@gmail.com>
2020-02-01 18:32:40 +0100

    Fix URIs

--- super-light-animated-scroll.php
+++ super-light-animated-scroll.php
@@ -1,7 +1,6 @@
 <?php
 /**
 * Plugin Name: Super Light Animated Scroll to id
-* Plugin URI: https://dlawp.pl/plugins
 * Description: Super light and easy to setup links that scroll the page smoothly to any id within the document.
 * Version: 0.1
 * Author: DlaWP.pl <support@dlaWP.pl>
